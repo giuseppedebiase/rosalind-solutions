128ca123fbad6948ba0d02f93ff3f02b2d3392b7
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-03-31 22:31:22 +0200

    Add files via upload

---
+++ Python Village/INI4/ini4.py
@@ -0,0 +1,18 @@
+file = open('rosalind_ini4.txt')
+contents = file.read().rstrip('\n')
+file.close()
+numbers = contents.split(' ')
+print(numbers)
+
+a = int(numbers[0])
+b = int(numbers[1])
+
+total = 0
+
+for i in range (a, b + 1):
+    if i % 2 != 0:
+        total += i
+
+output = open('rosalind_ini4_solution.txt', 'w')
+output.write(str(total))
+output.close()
