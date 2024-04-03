fbba4b0585dd1a0a93446abed73e338c21ad2c26
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-03 21:17:05 +0200

    Update dna.py

--- Bioinformatics Stronghold/DNA/dna.py
+++ Bioinformatics Stronghold/DNA/dna.py
@@ -1,3 +1,9 @@
+'''
+https://rosalind.info/problems/dna/
+
+Given: A DNA string s of length at most 1000 nt.
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+'''
 file = open('rosalind_dna.txt')
 dna = file.read().rstrip('\n')
 file.close()
