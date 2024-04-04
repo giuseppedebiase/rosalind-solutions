a000e7fc9968a1c917f9a54dc18e29d632e482e9
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-04 12:03:25 +0200

    Update rna.py

--- Bioinformatics Stronghold/RNA/rna.py
+++ Bioinformatics Stronghold/RNA/rna.py
@@ -1,3 +1,9 @@
+'''
+https://rosalind.info/problems/rna/
+
+Given: A DNA string t having length at most 1000 nt.
+Return: The transcribed RNA string of t.
+'''
 file = open('rosalind_rna.txt')
 dna = file.read()
 file.close()
