727b2458e12b782d2b7d88cf12dfcf86f580ed8b
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-05 12:23:23 +0200

    Update revc.py

--- Bioinformatics Stronghold/REVC/revc.py
+++ Bioinformatics Stronghold/REVC/revc.py
@@ -1,3 +1,9 @@
+'''
+https://rosalind.info/problems/revc/
+
+Given: A DNA string s of length at most 1000 bp.
+Return: The reverse complement sc of s.
+'''
 file = open('rosalind_revc.txt')
 dna = file.read()
 file.close()
