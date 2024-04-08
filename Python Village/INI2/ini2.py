5e718908054112f111bf3678aad634b9f2bdf184
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-08 13:03:21 +0200

    Update ini2.py

--- Python Village/INI2/ini2.py
+++ Python Village/INI2/ini2.py
@@ -1,3 +1,9 @@
+'''
+https://rosalind.info/problems/ini2/
+
+Given: Two positive integers a and b, each less than 1000.
+Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+'''
 file = open('rosalind_ini2.txt')
 contents = file.read().rstrip('\n')
 file.close()
