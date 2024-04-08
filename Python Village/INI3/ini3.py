669a36aa315ebd55708e15c095f3599f23bd289b
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-08 13:04:48 +0200

    Update ini3.py

--- Python Village/INI3/ini3.py
+++ Python Village/INI3/ini3.py
@@ -1,3 +1,9 @@
+'''
+https://rosalind.info/problems/ini3/
+
+Given: A string s of length at most 200 letters and four integers a, b, c and d.
+Return: The slice of this string from indices a through b and c through d (with space in between), inclusively. In other words, we should include elements s[b] and s[d] in our slice.
+'''
 file = open('rosalind_ini3.txt')
 contents = file.read()
 file.close()
