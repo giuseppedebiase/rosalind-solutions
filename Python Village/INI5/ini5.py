bf41f562ca4710e574177ddd7a82f1f131661220
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-11 12:28:35 +0200

    Update ini5.py

--- Python Village/INI5/ini5.py
+++ Python Village/INI5/ini5.py
@@ -1,3 +1,9 @@
+'''
+https://rosalind.info/problems/ini5/
+
+Given: A file containing at most 1000 lines.
+Return: A file containing all the even-numbered lines from the original file. Assume 1-based numbering of lines.
+'''
 file = open('rosalind_ini5.txt')
 output = open('rosalind_ini5_solution.txt', 'a')
 
