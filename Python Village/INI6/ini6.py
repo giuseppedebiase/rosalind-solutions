5f79d482b0c5f8b405d9b132c3d7770327e52852
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-11 12:29:26 +0200

    Update ini6.py

--- Python Village/INI6/ini6.py
+++ Python Village/INI6/ini6.py
@@ -1,3 +1,9 @@
+'''
+https://rosalind.info/problems/ini6/
+
+Given: A string s of length at most 10000 letters.
+Return: The number of occurrences of each word in s, where words are separated by spaces. Words are case-sensitive, and the lines in the output can be in any order.
+'''
 file = open('rosalind_ini6.txt')
 contents = file.read().rstrip('\n')
 file.close()
