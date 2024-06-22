141a1136e0461a03d0b588c01143e4267962047b
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-06-22 12:00:21 +0200

    Add files via upload

---
+++ Bioinformatics Stronghold/GC/gc_s.py
@@ -0,0 +1,45 @@
+#list is a list that contains each line of the input file
+list = []
+#headers is a list that contains every header of the input file
+headers = []
+
+file = open('rosalind_gc.txt')
+for line in file: 
+    #the solution requires the header to not have a > at the beginning
+    element = line.rstrip('\n').replace('>', '')
+    list.append(element)
+    if 'R' in element:
+        headers.append(element)
+file.close()
+
+'''
+each sequence is split among multiple lines in the .txt, 
+so I had to join all of the lines for each sequence
+to do that i joined every line of the file into one big string
+but replacing each header with the letter d
+this string will have each sequence preceded by the letter d: dAGCdTTG ecc.
+I then split this big string using .split('d')
+and then "popped" the first element (which is '')
+'''
+sequences = ''
+for i in list:
+    if 'R' in i:
+        sequences += 'd'
+    else:
+        sequences += i
+sequences = sequences.split('d')
+sequences.pop(0)
+
+#dict where key:value = GCcontent:Header
+dict = {}
+for i in range(len(headers)):
+    dict[((sequences[i].count('G') + sequences[i].count('C')) / len(sequences[i])) * 100] = headers[i]
+
+max = 0
+for key in dict:
+    if key > max:
+        max = key
+
+output = open('rosalind_gc_solution.txt', 'w')
+output.write(dict[max] + '\n' + str(max))
+output.close()
