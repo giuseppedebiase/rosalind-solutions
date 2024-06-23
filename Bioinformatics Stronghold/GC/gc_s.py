69d5d9cd94649b4a390244e929e8e864fd611cbc
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-06-23 14:19:43 +0200

    Update gc_s.py

--- Bioinformatics Stronghold/GC/gc_s.py
+++ Bioinformatics Stronghold/GC/gc_s.py
@@ -2,7 +2,8 @@
 https://rosalind.info/problems/gc/
 
 Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
-Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
+Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. 
+Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
 '''
 #list is a list that contains each line of the input file
 list = []
