dac4d975408c940e9344f22801eca89070c2bde4
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-07-25 09:32:55 +0200

    Update cons.py

--- Bioinformatics Stronghold/CONS/cons.py
+++ Bioinformatics Stronghold/CONS/cons.py
@@ -1,3 +1,9 @@
+'''
+https://rosalind.info/problems/cons/
+
+Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in FASTA format.
+Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist, then you may return any one of them.)
+'''
 file = open('rosalind_cons.txt')
 list = []
 for line in file:
