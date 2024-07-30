cb910109c9b929109ade9ecef2c271493c353418
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-07-30 11:57:03 +0200

    Update fibd.py

--- Bioinformatics Stronghold/FIBD/fibd.py
+++ Bioinformatics Stronghold/FIBD/fibd.py
@@ -1,3 +1,10 @@
+'''
+https://rosalind.info/problems/fibd/
+
+Given: Positive integers n≤100 and m≤20.
+
+Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
+'''
 file = open('rosalind_fibd.txt')
 contents = file.read()
 file.close()
